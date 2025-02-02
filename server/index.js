4c7e2075ea41e09a676a7c224c409cb6533ccfb0
Aniruddh-01 <u22cs088@coed.svnit.ac.in>
2025-02-02 09:09:46 +0530

    eploy error solved

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ const sql = neon(process.env.DB_URL);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(cors());
+app.use(cors({
+  origin : '*'
+}));
 
 app.get('/api/states', async (req, res) => {
     try {
